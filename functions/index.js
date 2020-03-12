f70acd17de2c5ca67dfa545fe2c8677548904c01
Yousef <yahmed1996a@gmail.com>
2020-03-12 21:40:08 +0000

    Setting up firebase done, testing functions and if connected to firestore

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+admin.initializeApp();
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from world!");
+});
+
+exports.getRunningDistanceKM = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('runningData').get()
+    .then(data =>{
+        runningData=[];
+        data.forEach(doc => {
+            runningData.push(doc.data());
+        })
+        return response.json(runningData);
+    });
+    .catch(err=>console.error(err));
+});
--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-analytics.js"></script>
+        <script></script>
       </header>
     </div>
   );
