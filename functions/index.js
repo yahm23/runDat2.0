2d9664104ece648e9dff8fbefc74e0b41d9cc24a
Yousef <yahmed1996a@gmail.com>
2020-03-17 14:08:36 +0000

    User authentication w/ email added! also reconfigured app: no config file, api key  stored in a .env file

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,29 @@
+// const firebaseConfig = require('../config.js')
 const functions = require('firebase-functions');
 var admin = require("firebase-admin");
 var serviceAccount = require("./runDatSDK.json");
+const app = require('express')();
+require('dotenv').config({ path: '../config.env' });
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://rundat-e0a41.firebaseio.com"
 });
+const firebase = require('firebase');
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURE_ID 
+  };
+
+firebase.initializeApp(config);
 
 
 app.get('/runningData', (req,res)=>{
@@ -36,7 +51,6 @@ app.post('/runningData',(req, res) => {
        Time:req.body.Time,
        userHandle:req.body.userHandle,
        DateRecorded: new Date().toISOString()
-
    };
    
     admin    
@@ -44,13 +58,31 @@ app.post('/runningData',(req, res) => {
     .collection('runningData')
     .add(newRunningData)
     .then((doc) =>{
-        res.json({message:`document ${doc.id} created successfully`});
+        res.json({message:`document ${doc.id} created successfully ${process.env.REACT_APP_API_KEY}`});
     })
     .catch(err=>{
         res.status(500).json({error:`Internal server error`});
         console.error(err);
     });
 });
+
+//Sign up route:
+app.post('/signup', (req,res)=>{
+    const newUser={
+        email:req.body.email,
+        password:req.body.password,
+        confirmPassword:req.body.confirmPassword,
+        handle:req.body.handle
+    };
+    firebase.auth().createUserWithEmailAndPassword(newUser.email,newUser.password)
+    .then(data=> {
+        return res.status(201).json({message:`user ${data.user.uid} has been signed up`})
+    })
+    .catch(err=>{
+        console.error(err);
+        res.status(500).json({error: err.code});
+    });
+})
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
  
